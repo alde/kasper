4e02b1a57030d3fdb37cb7153354a93d5ffb817f
Rickard Dybeck <r.dybeck@gmail.com>
2024-05-26 20:06:15 -0400

    add operator parsing

--- parsing.c
+++ parsing.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpc.h"
+#include <math.h>
 
 #ifdef _WIN32
 #include <string.h>
@@ -26,6 +27,36 @@ void add_history(char* unused) {}
 
 static char* VERSION = "0.0.1";
 
+long eval_op(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0) { return x - y; }
+	if (strcmp(op, "*") == 0) { return x * y; }
+	if (strcmp(op, "/") == 0) { return x / y; }
+	if (strcmp(op, "%") == 0) { return x % y; }
+	if (strcmp(op, "^") == 0) { return pow(x, y); }
+	return 0;
+}
+
+long eval(mpc_ast_t* t) {
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+
+	char* op = t->children[1]->contents;
+
+	long x = eval(t->children[2]);
+
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
+
+
+
 int main(int argc, char** argv) {
 	mpc_parser_t* Number = mpc_new("number");
 	mpc_parser_t* Operator = mpc_new("operator");
@@ -35,10 +66,10 @@ int main(int argc, char** argv) {
 	mpca_lang(MPCA_LANG_DEFAULT,
 		"									\
 			number		: /-?[0-9]+/ ;					\
-			operator	: '+' | '-' | '*' | '/' ;			\
+			operator	: '+' | '-' | '*' | '/' | '%' | '^' ;		\
 			expr		: <number> | '[' <operator> <expr>+ ']' ;	\
 			kasper		: /^/ <operator> <expr>+ /$/ ;			\
-		",Number, Operator, Expr, Kasper
+		", Number, Operator, Expr, Kasper
 	   );
 	
 	printf("kasper version %s\n", VERSION);
@@ -50,7 +81,8 @@ int main(int argc, char** argv) {
 
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Kasper, &r)) {
-			mpc_ast_print(r.output);
+			long result = eval(r.output);
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
 		} else {
 			mpc_err_print(r.error);
